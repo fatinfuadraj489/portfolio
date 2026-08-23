067f6653c629d33605cf8603f57ee8305936d768
fatinfuadraj489 <fatinfuadraj035@gmail.com>
2026-08-23 08:57:37 +0600

    Add files via upload

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -9,7 +9,7 @@ export default function Hero() {
 
         <h1 className="hero-title">
           I turn raw footage into<br />
-          <span className="accent-text">stories</span> that seles.
+          <span className="accent-text">stories</span> that lands.
         </h1>
 
         <p className="hero-sub">
